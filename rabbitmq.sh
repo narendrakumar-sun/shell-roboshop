b846810daa376642402f38a731996b3d12893207
Narendra <narendra.sunkara83@gmail.com>
2026-05-26 20:15:44 +0400

    Roboshop Instances

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -8,7 +8,7 @@
 
 USERID=$(id -u)
 LOG_FOLDER="/var/log/shell_script"
-LOF_FILE="$LOG_FOLDER/$0.log"
+LOG_FILE="$LOG_FOLDER/$0.log"
 SCRIPT_DIR=$pwd
 R="\e[31m"
 G="\e[32m"
@@ -16,17 +16,17 @@ Y="\e[33m"
 N="\e[0m"
 
 if [ $USERID -ne 0 ]; then
-   echo -e "$R Please run the script in ROOT user $N" | tee -a $LOF_FILE
+   echo -e "$R Please run the script in ROOT user $N" | tee -a $LOG_FILE
    exit 1
 fi
 mkdir -p $LOG_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-       echo -e "$2...........$R FAILURE $N" | tee -a $LOF_FILE
+       echo -e "$2...........$R FAILURE $N" | tee -a $LOG_FILE
        exit 1
     else
-       echo -e "$2..........$G SUCCESS $N" | tee -a $LOF_FILE
+       echo -e "$2..........$G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
