5c240bc8662df46f0280d87c52037c730104d3ad
Narendra <narendra.sunkara83@gmail.com>
2026-05-28 13:28:30 +0400

    Roboshop Instances

--- redis.sh
+++ redis.sh
@@ -1,4 +1,43 @@
 #!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable redis -y &>>$LOGS_FILE
+dnf module enable redis:7 -y &>>$LOGS_FILE
+VALIDATE $? "Enable Redis:7"
+
+dnf install redis -y  &>>$LOGS_FILE
+VALIDATE $? "Installed Redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Allowing remote connections"
+
+systemctl enable redis &>>$LOGS_FILE
+systemctl start redis 
+VALIDATE $? "Enabled and started Redis"
+
 #####################################
 # Author : narendra
 # Date : 25/5/2026
@@ -6,41 +45,43 @@
 # Program : Create Redis Database
 #####################################
 
-USERID=$(id -u)
-LOG_FOLDER="/var/log/shell_script"
-LOF_FILE="$LOG_FOLDER/$0.log"
-SCRIPT_DIR=$pwd
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+# USERID=$(id -u)
+# LOG_FOLDER="/var/log/shell_script"
+# LOF_FILE="$LOG_FOLDER/$0.log"
+# SCRIPT_DIR=$pwd
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
 
-if [ $USERID -ne 0 ]; then
-   echo -e "$R Please run the script in ROOT user $N" | tee -a $LOF_FILE
-   exit 1
-fi
-mkdir -p $LOG_FOLDER
+# if [ $USERID -ne 0 ]; then
+#    echo -e "$R Please run the script in ROOT user $N" | tee -a $LOF_FILE
+#    exit 1
+# fi
+# mkdir -p $LOG_FOLDER
 
-VALIDATE(){
-    if [ $1 -ne 0 ]; then
-       echo -e "$2...........$R FAILURE $N" | tee -a $LOF_FILE
-       exit 1
-    else
-       echo -e "$2..........$G SUCCESS $N" | tee -a $LOF_FILE
-    fi
-}
+# VALIDATE(){
+#     if [ $1 -ne 0 ]; then
+#        echo -e "$2...........$R FAILURE $N" | tee -a $LOF_FILE
+#        exit 1
+#     else
+#        echo -e "$2..........$G SUCCESS $N" | tee -a $LOF_FILE
+#     fi
+# }
 
-dnf module disable redis -y
-dnf module enable redis:7 -y &>>$LOF_FILE
-VALIDATE $? "Enable Redis"
+# dnf module disable redis -y
+# dnf module enable redis:7 -y &>>$LOF_FILE
+# VALIDATE $? "Enable Redis"
 
-dnf install redis -y &>>$LOF_FILE
-VALIDATE $? "Installing redis"
+# dnf install redis -y &>>$LOF_FILE
+# VALIDATE $? "Installing redis"
 
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-VALIDATE $? " Allowing remote connection"
+# sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+# VALIDATE $? " Allowing remote connection"
 
-systemctl enable redis &>>$LOF_FILE
-systemctl start redis 
-VALIDATE $? "Enable and Start REDIS"
+# systemctl enable redis &>>$LOF_FILE
+# systemctl start redis 
+# VALIDATE $? "Enable and Start REDIS"
+
+
