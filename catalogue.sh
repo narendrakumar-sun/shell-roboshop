3760abadbea6742574606efafee92d2ee70b17e0
Narendra <narendra.sunkara83@gmail.com>
2026-05-28 11:55:01 +0400

    Roboshop Instances

--- catalogue.sh
+++ catalogue.sh
@@ -50,7 +50,7 @@ else
     echo -e "Roboshop user already exist ... $Y SKIPPING $N"
 fi
 
-mkdir /app 
+mkdir -p /app 
 VALIDATE $? " Creating app directory"
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
